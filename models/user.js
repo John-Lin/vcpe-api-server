9d675afa4b9f6a869ebec08fe7e1c129e644e820
John-Lin <linton.tw@gmail.com>
2015-12-22 16:28:48 +0800

    node-config support

--- models/user.js
+++ models/user.js
@@ -1,15 +1,19 @@
 'use strict';
 const vogels = require('vogels');
 const Joi = require('joi');
+const config = require('config');
+const debug = require('debug')('configure');
 
 // Loading credentials from JSON file
 // vogels.AWS.config.loadFromPath(`./config/credentials.json`);
 
 vogels.AWS.config.update({
-  accessKeyId: process.env.AWS_ACCESS_KEY_ID,
-  secretAccessKey: process.env.AWS_SECRET_ACCESS_KEY,
-  region: process.env.AWS_REGION || 'us-east-1',
+  accessKeyId: config.get('AWS_CREDENTIALS.accessKeyId') || process.env.AWS_ACCESS_KEY_ID,
+  secretAccessKey: config.get('AWS_CREDENTIALS.secretAccessKey') ||  process.env.AWS_SECRET_ACCESS_KEY,
+  region: config.get('AWS_CREDENTIALS.region') || process.env.AWS_REGION,
 });
+
+debug(`Now in ${process.env.NODE_ENV} mode!`);
 
 let Account = vogels.define('Account', {
   hashKey: 'email',
