600dc08ef253709b3a14e5651ae2e84a007f17b7
John-Lin <linton.tw@gmail.com>
2015-12-24 17:46:48 +0800

    update router detail settings and status

--- routes/settings.js
+++ routes/settings.js
@@ -1,5 +1,5 @@
 'use strict';
-const debug = require('debug')('models-settings');
+const debug = require('debug')('models');
 let express = require('express');
 let util = require('util');
 let _ = require('lodash');
@@ -7,40 +7,109 @@ let Account = require('../models/user.js');
 let router = express.Router();
 
 /*
- * PUT to update(turn on/off) NAT status.
+ * PUT to update(turn on/off) router status.
  */
-router.put('/router', (req, res) => {
+router.put('/router/status', (req, res) => {
   req.accepts(['application/json']);
+  let username = req.body.username;
+  let routerStatus = req.body.routerStatus;
 
-  if (!(req.body.natStatus)) {
-    res.sendStatus(400);
+  if (!(routerStatus && username)) {
+    res.status(400).json({error: 'routerStatus or username should be set.'});
     return;
   }
 
-  let params = {
-    email: req.body.email,
-    settings: {
-      containerid: 'c01b39c7a35ccc3b081a3e83d2c71fa9a767ebfeb45c6',
-      nickname: 'mycontainer',
-      router: {
-        routerStatus: 'OFF',
-        routerWANPort: 1,
-        routerPublicIP: '140.114.99.189',
-        routerDefultGateway: '140.114.99.254',
-        routerLocalNetwork: '192.168.8.0',
-      },
-    },
-  };
+  Account
+    .query(username)
+    .exec((err, resp) => {
+      if (err) debug(err);
 
-  // By default overwrite is set to true,
-  // allowing create operations to overwrite existing records
-  Account.create(params, (err, acc) => {
-    if (err) debug(err);
-    debug(`${acc.get('email')} created an account at ${acc.get('createdAt')}`);
-    res.status(201).json({
-      uuid: acc.get('vCPEUUID'),
+      if (resp.Count === 0) {
+        res.status(204).json({});
+        return;
+      }
+
+      // retrieve settings data
+      let settings = (_.pluck(resp.Items, 'attrs'))[0].settings;
+
+      // updated the router status
+      settings.router.status = routerStatus;
+
+      // Save into db
+      Account.update({
+        username: username,
+        settings: settings,
+      }, function(err, acc) {
+        if (err) debug(err);
+        res.status(200).json({});
+      });
+
     });
-  });
+});
+
+/*
+ * PUT to update router detail settings.
+ */
+router.put('/router/detail', (req, res) => {
+  req.accepts(['application/json']);
+  let username = req.body.username;
+  let routerWANPort = req.body.routerWANPort;
+  let routerPublicIP = req.body.routerPublicIP;
+  let routerDefultGateway = req.body.routerDefultGateway;
+  let routerLocalNetwork = req.body.routerLocalNetwork;
+
+  if (!(username)) {
+    res.status(400).json({error: 'username should be set.'});
+    return;
+  }
+
+  Account
+    .query(username)
+    .exec((err, resp) => {
+      if (err) debug(err);
+
+      if (resp.Count === 0) {
+        res.status(204).json({});
+        return;
+      }
+
+      // retrieve settings data
+      let settings = (_.pluck(resp.Items, 'attrs'))[0].settings;
+
+      // updated the router detail settings.
+      settings.router.wanPort = routerWANPort ?
+        routerWANPort :
+        settings.router.wanPort;
+
+      settings.router.publicIP = routerPublicIP ?
+        routerPublicIP :
+        settings.router.publicIP;
+
+      settings.router.defultGateway = routerDefultGateway ?
+        routerDefultGateway :
+        settings.router.defultGateway;
+
+      settings.router.localNetwork = routerLocalNetwork ?
+        routerLocalNetwork :
+        settings.router.localNetwork;
+
+      // Save into db
+      Account.update({
+        username: username,
+        settings: settings,
+      }, function(err, acc) {
+        if (err) debug(err);
+        res.status(200).json({});
+      });
+
+    });
+
+});
+
+/*
+ * PUT to update container settings.
+ */
+router.put('/container', (req, res) => {
 
 });
 
